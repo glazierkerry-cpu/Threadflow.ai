76cc76ce3684c96b15bf112f2696b508d5778f0c
glazierkerry-cpu <glazier.kerry@gmail.com>
2026-08-16 22:29:40 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, send_file
+from rembg import remove
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+@app.route('/remove-bg', methods=['POST'])
+def remove_background():
+    file = request.files['image']
+    input_image = Image.open(file.stream)
+    output_image = remove(input_image)
+    
+    img_io = io.BytesIO()
+    output_image.save(img_io, 'PNG')
+    img_io.seek(0)
+    
+    return send_file(img_io, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
